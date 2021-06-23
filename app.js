8bfb03e689bb58c8a9a9eaa5106ac65ec746804f
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2021-06-23 14:06:02 +0530

    favicon added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/favicon.ico", express.static("favicon.ico"));
 const mongoose = require("mongoose");
 // mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 mongoose.connect("mongodb://localhost:27017/reviewsDB", {
